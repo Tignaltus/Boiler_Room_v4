d2766fda0f4947f48ee6d13838c374d1ed1526b9
kostas900402 <nascoskostas@gmail.com>
2025-10-07 09:27:24 +0200

    kostas commit first

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,9 +2,9 @@ public class Customer
 {
     private String name;
     private String city;
-    private float orderValue;
+    private double orderValue;
 
-    public Customer(String name, String city, float orderValue)
+    public Customer(String name, String city, double orderValue)
     {
         this.name = name;
         this.city = city;
@@ -23,10 +23,32 @@ public class Customer
         return city;
     }
 
-    public float getOrderValue()
+    public double getOrderValue()
     {
         return orderValue;
     }
 
     //</editor-fold>
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setOrderValue(double orderValue) {
+        this.orderValue = orderValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", orderValue=" + orderValue +
+                '}';
+    }
 }
--- src/main/java/customerCSV.java
+++ src/main/java/customerCSV.java
@@ -1,16 +1,40 @@
-import java.util.Scanner;
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class customerCSV
 {
     Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
-        new customerCSV().run();
-    }
+    public static void main(String[] args) throws IOException {
 
-    public void run()
-    {
-        
-    }
+        List<Customer> customers = Arrays.asList(
+                new Customer("Jonatan", "Lund", 24000),
+                new Customer("Felix", "Dalby", 4000),
+                new Customer("Rasha", "Malmö", 2000),
+                new Customer("Kostas", "Lund", 200));
+
+
+
+//.sorted(Comparator.comparing(Customer::getOrderValue).reversed())
+    List<String> topCustomer = customers.stream()
+            .filter(c -> c.getOrderValue()>=1000)
+            .sorted((val1, val2)-> (int) (val2.getOrderValue() - val1.getOrderValue()))
+            .limit(10)
+            .map(c -> "name: " + c.getName() + " city: " + c.getCity() + " order value: " + c.getOrderValue() + "\n")
+            .collect(Collectors.toList());
+
+        System.out.println(topCustomer);
+
+
+        Path filepath = Paths.get("report.txt");
+        Files.write(filepath, topCustomer);
+
+
+        }
 }
